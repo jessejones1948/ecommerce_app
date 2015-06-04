1db0f434cf3454e06b9d971085f74576c1f8d31b
jessejones1948 <olathanbake1@yahoo.com>
2015-06-04 08:54:51 -0400

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,33 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  before_filter :categories
+  before_filter :brands
+
+  def categories
+  	@categories = Category.all
+  end
+
+  def brands
+
+  	@brands = []
+  	Product.all.each do |product|
+  		if @brands.include?(product.brand) == false
+  			#unless @brands.include?(product.brand)
+  			@brands.push(product.brand)
+  		end
+  	end
+  	return @brands
+  end
+
+  def remote_ip
+    if request.remote_ip == '127.0.0.1'
+      #Hard coded remote address
+      "#{ENV['my_url']}"
+    else
+      request.remote_ip
+    end
+  end
+end
